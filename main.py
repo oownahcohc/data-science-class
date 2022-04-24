54fc11ffb150f35be62ae09eafacd6cad9baad08
chanwoo <chocw0402@gmail.com>
2022-04-24 19:06:12 +0900

    [MOVE]

--- main.py
+++
@@ -1,9 +0,0 @@
-import time
-from selenium import webdriver
-from webdriver_manager.chrome import ChromeDriverManager
-
-driver = webdriver.Chrome(ChromeDriverManager().install())
-CoffeeBean_URL = "https://www.coffeebeankorea.com/store/store.asp"
-
-driver.get(CoffeeBean_URL)
-time.sleep(1)
